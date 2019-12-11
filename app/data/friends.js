bd33ef01853f0d82a064914256f41b618f8b3fb6
Ajay Singh <ajayh2@gmail.com>
2019-12-11 16:33:15 -0500

    harry potter charcters

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var friends = [
+  {
+    name: "Harry Potter",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/en/d/d7/Harry_Potter_character_poster.jpg",
+    scores: ["5", "1", "4", "3", "1", "5", "1", "4", "3", "1"]
+  },
+  {
+    name: "Hermione Granger",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/en/d/d3/Hermione_Granger_poster.jpg",
+    scores: ["3", "1", "5", "4", "1", "5", "1", "1", "5", "3"]
+  },
+  {
+    name: "Ron Weasley",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/en/5/5e/Ron_Weasley_poster.jpg",
+    score: ["3", "1", "4", "1", "4", "3", "5", "1", "5", "4"]
+  }
+];
+
+module.exports = friends;
