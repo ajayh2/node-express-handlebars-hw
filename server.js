f8c14b89089f48b820f40047f1acef93c2f4ac9a
Ajay Singh <ajayh2@gmail.com>
2019-12-11 21:40:05 -0500

    deployed

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // ==============================================================================
 
 var express = require("express");
+var bodyParser = require("body-parser");
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
@@ -17,8 +18,8 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // ================================================================================
 // ROUTER
@@ -26,8 +27,8 @@ app.use(express.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
